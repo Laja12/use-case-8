e8d43b986b37dce2cca6fc1f8def814ce922094e
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-23 16:02:39 +0530

    Update outputs.tf

--- modules/destination_bucket/outputs.tf
+++ modules/destination_bucket/outputs.tf
@@ -1,9 +1,4 @@
-# modules/s3_bucket/main.tf
-resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
-}
-
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
+# modules/s3_bucket/outputs.tf
+output "bucket_name" {
+  value = aws_s3_bucket.this.bucket
 }
