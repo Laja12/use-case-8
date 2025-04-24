9d56593905653069c88f41648a8a7bee78158a81
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-24 09:14:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.0.0"
   backend "s3" {
     bucket = "openproject-bucket1"  # Replace with your unique S3 bucket name
     key    = "path/image/my-bucket2/statefile/terraform.tfstate"  # Path inside the bucket to store the state
